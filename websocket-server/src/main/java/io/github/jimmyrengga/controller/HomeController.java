007b53e50fd31cd661182296e779f7137ef11b38
jimmy rengga <rengga.jimmy@gmail.com>
2015-11-09 12:08:20 +0700

    tambah API untuk kirim message ke user

--- websocket-server/src/main/java/io/github/jimmyrengga/controller/HomeController.java
+++ websocket-server/src/main/java/io/github/jimmyrengga/controller/HomeController.java
@@ -1,27 +1,29 @@
 package io.github.jimmyrengga.controller;
 
+import io.github.jimmyrengga.Message;
+import io.github.jimmyrengga.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
  * @author jimmy
  */
 @Controller
+@RequestMapping("/api/message")
 public class HomeController {
 
-//    @Autowired MessageService messageService;
-//    
-//    @RequestMapping("/")
-//    public String index(Message message) {
-//        return "index";
-//    }
-//
-//    @RequestMapping(value = "/", method = RequestMethod.POST)
-//    public String broadcastMessage(@ModelAttribute("message") Message message,
-//            BindingResult result) {
-//        messageService.broadcastToUser(message);
-//        
-//        return "index";
-//    }
-    
+    @Autowired MessageService messageService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void sendToUser(@RequestBody Message message) {
+        if(message == null) {
+            throw new RuntimeException("Message must not be null");
+        }
+        
+        messageService.broadcastToUser(message);
+    }
 }
